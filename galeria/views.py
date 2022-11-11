d987e33ec6ae5a48cb1f8e1e58ebac658a1c50d3
Yan Julião <yanjuliao7@gmail.com>
2022-11-11 15:27:04 -0300

    Implementação das imagens

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'galeria/index.html')
 
 def imagem(request):
-    return(request, 'galeria/imagem.html')
+    return render(request, 'galeria/imagem.html')
